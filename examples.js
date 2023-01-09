4a8de57ba098429480659d2b913670ee63efe25d
Poptamas Vlad <poptamasvlad@gmail.com>
2023-01-09 16:08:55 +0200

    extract inline js into examples.js

---
+++ examples.js
@@ -0,0 +1,15 @@
+console.info("page Loaded");
+console.debug(2 + 3);
+
+var age;
+var employed = false;
+console.warn(employed);
+
+var skills = ["html", "css", "js", "c++", "c#"];
+
+var vlad = {
+  age: 21,
+  employed: false,
+};
+
+var jobTitle = document.getElementById("job-title");
