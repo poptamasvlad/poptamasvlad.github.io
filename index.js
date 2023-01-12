2a3efea9f7a04e7e931141a67f07e58da00dccc7
Poptamas Vlad <poptamasvlad@gmail.com>
2023-01-12 19:28:29 +0200

    use array.forEach helper

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ for (var i = 0; i < skills.length; i++) {
 
 skillsEl.innerHTML = skillsHtml;
 
+var pageIds = ["home", "skills", "languages", "projects"];
+
+function hidePage(id) {
+  hide(id);
+}
+
 function hideAllPages() {
-  hide("home");
-  hide("skills");
-  hide("languages");
-  hide("projects");
+  pageIds.forEach(hidePage);
 }
 
 function show(id) {
